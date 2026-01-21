c3b9f15b8aa588e66131af8b6a5e85552271f854
Bartek Smolkowski <bartek.smolkowski@gmail.com>
2026-01-21 17:10:36 +0100

    nauka class_rebuilder

--- a00_System_Baza/baza_nauki.py
+++ a00_System_Baza/baza_nauki.py
@@ -1,16 +1,15 @@
 import os
 import shutil
 from functools import wraps
-from typing import Optional
 
 __all__ = [
     "__BazaNauki__", 
-    "AutoDekoruj", 
-    "bezpieczny_wrapper"
+    "bezpieczny_wrapper",
+    "dekoruj_wszystko"
 ]
 
 class __BazaNauki__:
-    def __init__(self, aktywne=True, dekoratory: Optional[list] = None):
+    def __init__(self, aktywne=True):
         if not aktywne:
             print(f">>> SEKCJA POMINIĘTA: {self.__class__.__name__}")
             return
@@ -20,59 +19,6 @@ class __BazaNauki__:
             wartosc = getattr(self, nazwa)
             if callable(wartosc) and not nazwa.startswith("_") and nazwa != "run":
                 wartosc()
-
-
-class AutoDekoruj(type):
-    """
-    METAKLASA – KLASA, KTÓRA TWORZY INNE KLASY
-    Python działa tak:
-        obiekt  ← tworzony przez klasę
-        klasa   ← tworzona przez metaklasę
-
-    Ten __new__ uruchamia się W MOMENCIE TWORZENIA KLASY,
-    a NIE podczas tworzenia jej instancji.
-
-    Parametry __new__ metaklasy:
-
-    cls:
-        Metaklasa (np. AutoDekoruj), która buduje nową klasę.
-
-    name:
-        Nazwa nowo tworzonej klasy jako string.
-        Przykład: "FileManager"
-
-    bases:
-        Krotka klas bazowych.
-        Przykład: (__BazaNauki__,)
-
-    metody_i_atrybuty (dct):
-        Słownik zawierający CAŁE CIAŁO KLASY:
-        - metody (funkcje)
-        - atrybuty klasowe
-        - dekoratory
-        - zmienne
-
-        Przykład:
-        {
-            'list_files': <function ...>,
-            'create_directory': <function ...>,
-            '__module__': 'file_manager',
-            '__doc__': None
-        }
-
-    Dzięki metaklasie możemy:
-        - automatycznie dekorować metody
-        - modyfikować klasę bez pisania dekoratora na każdej metodzie
-        - narzucać reguły całej klasie
-    """
-    def __new__(cls, name, base, dct):
-        print(f"cls:\n{cls}")
-        print(f"name:\n{name}")
-        print(f"base:\n{base}")
-        print(f"dct:\n{dct}")
-
-        for k, v in dct.items():
-            0
 
 def bezpieczny_wrapper(func):
     @wraps(func)
@@ -89,3 +35,13 @@ def bezpieczny_wrapper(func):
 
         return func(self, *args, **kwargs)
     return wrapper
+
+def dekoruj_wszystko(*dekoratory):
+    def class_rebuilder(cls: type):
+        for nazwa, wartosc in vars(cls).items():
+            if callable(wartosc) and not nazwa.startswith("__"):
+                for d in dekoratory:
+                    wartosc = d(wartosc)
+                setattr(cls, nazwa, wartosc)
+        return cls
+    return class_rebuilder
