b581f8ae4e403d97b0bc3878bf0f0286b00234b3
Bartek Smolkowski <bartek.smolkowski@gmail.com>
2026-01-26 17:41:27 +0100

    baza_nauki - mozliwosc wybierania metod w __BazaNauki__ i odsuniecia na bok

--- a00_System_Baza/baza_nauki.py
+++ a00_System_Baza/baza_nauki.py
@@ -1,20 +1,38 @@
 import os
 import shutil
 from functools import wraps
+from typing import Optional
 
 __all__ = ["__BazaNauki__", "bezpieczny_wrapper", "dekoruj_wszystko"]
 
 
 class __BazaNauki__:
-    def __init__(self, aktywne=True):
+    def __init__(
+        self,
+        aktywne=True,
+        metody: Optional[list] = None,
+        na_odwrot_metody: bool = False,
+    ):
         if not aktywne:
             print(f">>> SEKCJA POMINIĘTA: {self.__class__.__name__}")
             return
 
+        wszystkie_publiczne = [
+            n
+            for n in dir(self)
+            if callable(getattr(self, n)) and not n.startswith("_") and n != "run"
+        ]
+
+        wybrane_metody = metody if metody is not None else wszystkie_publiczne
+
         print(f"\n>>> URUCHAMIAM SEKCJĘ: {self.__class__.__name__} <<<")
-        for nazwa in dir(self):
+
+        for nazwa in wszystkie_publiczne:
             wartosc = getattr(self, nazwa)
-            if callable(wartosc) and not nazwa.startswith("_") and nazwa != "run":
+
+            czy_uruchomic = (nazwa in wybrane_metody) != na_odwrot_metody
+
+            if czy_uruchomic:
                 wartosc()
 
 
