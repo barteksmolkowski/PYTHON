c42c471c25f348108112da907310677fb42ccf96
Bartek Smolkowski <bartek.smolkowski@gmail.com>
2026-01-26 17:39:19 +0100

    init - automatyczne tworzenie __all__

--- a00_System_Baza/__init__.py
+++ a00_System_Baza/__init__.py
@@ -1,3 +1,30 @@
-from .baza_nauki import AutoDekoruj, __BazaNauki__, bezpieczny_wrapper
+import types
 
-__all__ = ["__BazaNauki__", "AutoDekoruj", "bezpieczny_wrapper"]
+from .baza_nauki import *
+
+base_attrs = dir(types.ModuleType("base"))
+base_attrs.extend(
+    [
+        "__annotations__",
+        "__builtins__",
+        "__file__",
+        "__cached__",
+        "types",
+        "__path__",
+        "__loader__",
+        "__spec__",
+    ]
+)
+
+imported_names = [
+    n
+    for n in locals()
+    if n not in base_attrs and n != "base_attrs" and (n.startswith("__") or "_" in n)
+]
+
+__all__ = imported_names
+
+if "base_attrs" in locals():
+    del base_attrs
+if "imported_names" in locals():
+    del imported_names
--- a01_Nauka_Aktualna/__init__.py
+++ a01_Nauka_Aktualna/__init__.py
@@ -1,3 +1,30 @@
-from a00_System_Baza import AutoDekoruj, __BazaNauki__, bezpieczny_wrapper
+import types
 
-__all__ = ["AutoDekoruj", "__BazaNauki__", "bezpieczny_wrapper"]
+from a00_System_Baza import *
+
+base_attrs = dir(types.ModuleType("base"))
+base_attrs.extend(
+    [
+        "__annotations__",
+        "__builtins__",
+        "__file__",
+        "__cached__",
+        "types",
+        "__path__",
+        "__loader__",
+        "__spec__",
+    ]
+)
+
+imported_names = [
+    n
+    for n in locals()
+    if n not in base_attrs and n != "base_attrs" and (n.startswith("__") or "_" in n)
+]
+
+__all__ = imported_names
+
+if "base_attrs" in locals():
+    del base_attrs
+if "imported_names" in locals():
+    del imported_names
