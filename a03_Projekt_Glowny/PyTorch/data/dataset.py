cb2d4da0c3e69a03b95c54a38f793d2216845f17
Bartek Smolkowski <bartek.smolkowski@gmail.com>
2026-01-31 20:42:05 +0100

    dataset - dodanie protokołów i aliasów typów

--- a03_Projekt_Glowny/PyTorch/data/dataset.py
+++ a03_Projekt_Glowny/PyTorch/data/dataset.py
@@ -1,19 +1,26 @@
-from .common import ABC, abstractmethod
+from typing import Protocol, Union, runtime_checkable
+
+import numpy as np
+
+Mtx = np.ndarray
+MtxList = list[np.ndarray]
 
 
-class __Dataset__(ABC):
-    @abstractmethod
+class DatasetProtocol(Protocol):
+    def __len__(self) -> int: ...
+
+    def __getitem__(self, index: int) -> Union[Mtx, MtxList]: ...
+
+
+class Dataset:
     def __len__(self) -> int:
-        pass
+        """
+        zwraca całkowitą liczbę próbek w zbiorze danych
+        """
+        return 0
 
-    @abstractmethod
-    def __getitem__(self, index: int):
-        pass
-
-
-class Dataset(__Dataset__):
-    def __len__(self):
-        0
-
-    def __getitem__(self, index):
-        0
+    def __getitem__(self, index: int) -> Union[Mtx, MtxList]:
+        """
+        pobiera pojedynczą macierz lub listę macierzy dla danego indeksu
+        """
+        return np.array([])
