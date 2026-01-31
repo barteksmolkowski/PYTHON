8eb5c30bc1fb4374cddf2cd4de3b7916b5cbf28f
Bartek Smolkowski <bartek.smolkowski@gmail.com>
2026-01-31 19:51:30 +0100

    common_utils - restart pliku i wdrożenie metody

---
+++ a03_Projekt_Glowny/PyTorch/common_utils.py
@@ -0,0 +1,26 @@
+import types
+
+
+def build_all(local_vars: dict) -> list[str]:
+    base_attrs = dir(types.ModuleType("base"))
+    base_attrs.extend(
+        [
+            "__annotations__",
+            "__builtins__",
+            "__file__",
+            "__cached__",
+            "types",
+            "__path__",
+            "__loader__",
+            "__spec__",
+            "base_attrs",
+        ]
+    )
+
+    return [
+        n
+        for n, obj in local_vars.items()
+        if n not in base_attrs
+        and not isinstance(obj, types.ModuleType)
+        and (n.startswith("__") or "_" in n or n.isupper())
+    ]
