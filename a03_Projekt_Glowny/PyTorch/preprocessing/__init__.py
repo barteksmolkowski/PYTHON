ccd35c471471d70cdcab33b18fcb45caa987b0ee
Bartek Smolkowski <bartek.smolkowski@gmail.com>
2026-02-01 20:54:27 +0100

    __init__ - naprawa ostrzeżeń o nieużywanych importach w initach

--- a03_Projekt_Glowny/PyTorch/preprocessing/__init__.py
+++ a03_Projekt_Glowny/PyTorch/preprocessing/__init__.py
@@ -1,15 +1,16 @@
 from common_utils import build_all
 
-from .augmentation import DataAugmentation
-from .conversion import ImageToMatrixConverter
-from .convolution import ConvolutionActions
+from .augmentation import DataAugmentation as DataAugmentation
+from .conversion import ImageToMatrixConverter as ImageToMatrixConverter
+from .convolution import ConvolutionActions as ConvolutionActions
 from .decorators import *
-from .geometry import ImageGeometry
-from .grayscale import GrayScaleProcessing
-from .io_image import ImageHandler
-from .normalization import Normalization
-from .pipeline import ImageDataPreprocessing, TransformPipeline
-from .pooling import Pooling
-from .thresholding import Thresholding
+from .geometry import ImageGeometry as ImageGeometry
+from .grayscale import GrayScaleProcessing as GrayScaleProcessing
+from .io_image import ImageHandler as ImageHandler
+from .normalization import Normalization as Normalization
+from .pipeline import ImageDataPreprocessing as ImageDataPreprocessing
+from .pipeline import TransformPipeline as TransformPipeline
+from .pooling import Pooling as Pooling
+from .thresholding import Thresholding as Thresholding
 
 __all__ = build_all(locals())
