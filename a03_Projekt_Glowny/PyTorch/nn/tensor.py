bf32908fb2d33ca64c9ee80e3fa902aa0ab5cbaa
Bartek Smolkowski <bartek.smolkowski@gmail.com>
2026-02-01 20:50:44 +0100

    tensor - validate_shape 100%

--- a03_Projekt_Glowny/PyTorch/nn/tensor.py
+++ a03_Projekt_Glowny/PyTorch/nn/tensor.py
@@ -6,5 +6,10 @@ Mtx: TypeAlias = np.ndarray
 Tensor: TypeAlias = np.ndarray
 Shape: TypeAlias = Tuple[int, ...]
 
-def validate_shape(data: Mtx, expected: Shape) -> bool:
-    ...
+
+def validate_shape(data: Mtx, expected: Shape) -> None:
+    if data.shape != expected:
+        raise ValueError(
+            f"Dimensions error! Received {data.shape}, expected {expected}."
+            "Check the Linear layer or image size."
+        )
