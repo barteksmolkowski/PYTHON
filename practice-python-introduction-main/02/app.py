177a16e6548fec51d6af4ccb6b93155d6de53290
Bartek Smolkowski <bartek.smolkowski@gmail.com>
2026-02-16 17:23:12 +0100

    ostatnia poprawka dodanie if __name__ == "__main__"

--- practice-python-introduction-main/02/app.py
+++ practice-python-introduction-main/02/app.py
@@ -1,9 +1,5 @@
-"""
-Utwórz jakąś zmienną, napisz instrukcję warunkową if,
-dopisz komentarz jedno oraz wieloliniowy oraz wyświetl
-jakiś napisa w terminalu - użyj funkcji print().
-"""
-x = 1
-x = 2 if x == 1 else 1
-# komentarz?
-print(x)
+if __name__ == "__main__":
+    x = 1
+    x = 2 if x == 1 else 1
+    # komentarz?
+    print(x)
