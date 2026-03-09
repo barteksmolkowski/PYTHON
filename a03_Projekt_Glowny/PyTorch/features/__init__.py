7e03be12faffbc39207b7e5d0d3c23f6c8a7a20d
Bartek Smolkowski <bartek.smolkowski@gmail.com>
2026-03-09 19:46:17 +0100

    __init__ - expose logic in __init__

--- a03_Projekt_Glowny/PyTorch/features/__init__.py
+++ a03_Projekt_Glowny/PyTorch/features/__init__.py
@@ -1,6 +1,17 @@
-from .edges import EdgeDetectorProtocol, Prewitt, Sobel
-from .extractor import FeatureExtraction, FeatureExtractorProtocol
-from .hog import HOG, HOGProtocol
+from .edges import (
+    EdgeDetectorProtocol,
+    Prewitt,
+    Sobel,
+    apply_prewitt_filter_logic,
+    apply_sobel_filter_logic,
+)
+from .extractor import (
+    FeatureExtraction,
+    FeatureExtractorProtocol,
+    extract_edges_logic,
+    extract_features_vector_logic,
+)
+from .hog import HOG, HOGProtocol, compute_hog_descriptor_logic
 
 __all__ = [
     "HOG",
@@ -10,4 +21,9 @@ __all__ = [
     "HOGProtocol",
     "Prewitt",
     "Sobel",
+    "apply_prewitt_filter_logic",
+    "apply_sobel_filter_logic",
+    "compute_hog_descriptor_logic",
+    "extract_edges_logic",
+    "extract_features_vector_logic",
 ]
