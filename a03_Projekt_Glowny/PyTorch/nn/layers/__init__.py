d0b5aced199283bdfe0dd576abc39850c7eeee2f
Bartek Smolkowski <bartek.smolkowski@gmail.com>
2026-03-10 18:48:26 +0100

    __init__ - expose fwd/bwd logic and minify layer names

--- a03_Projekt_Glowny/PyTorch/nn/layers/__init__.py
+++ a03_Projekt_Glowny/PyTorch/nn/layers/__init__.py
@@ -1,17 +1,57 @@
-from .activation import ReLULayer, SigmoidLayer, SoftmaxLayer
+from .activation import (
+    ReLU,
+    Sigmoid,
+    Softmax,
+    relu_bwd,
+    relu_fwd,
+    sigmoid_bwd,
+    sigmoid_fwd,
+    softmax_bwd,
+    softmax_fwd,
+)
 from .base import LayerProtocol
-from .conv_layer import Conv2DLayer
-from .dropout import DropoutLayer
-from .flatten import FlattenLayer
-from .linear import LinearLayer
+from .conv_layer import (
+    Conv2D,
+    conv2d_bwd,
+    conv2d_fwd,
+)
+from .dropout import (
+    Dropout,
+    dropout_bwd,
+    dropout_fwd,
+)
+from .flatten import (
+    Flatten,
+    flatten_bwd,
+    flatten_fwd,
+)
+from .linear import (
+    Linear,
+    linear_bwd,
+    linear_fwd,
+)
 
 __all__ = [
-    "Conv2DLayer",
-    "DropoutLayer",
-    "FlattenLayer",
+    "Conv2D",
+    "Dropout",
+    "Flatten",
     "LayerProtocol",
-    "LinearLayer",
-    "ReLULayer",
-    "SigmoidLayer",
-    "SoftmaxLayer",
+    "Linear",
+    "ReLU",
+    "Sigmoid",
+    "Softmax",
+    "conv2d_bwd",
+    "conv2d_fwd",
+    "dropout_bwd",
+    "dropout_fwd",
+    "flatten_bwd",
+    "flatten_fwd",
+    "linear_bwd",
+    "linear_fwd",
+    "relu_bwd",
+    "relu_fwd",
+    "sigmoid_bwd",
+    "sigmoid_fwd",
+    "softmax_bwd",
+    "softmax_fwd",
 ]
